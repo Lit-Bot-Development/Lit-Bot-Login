48f7da312f4531b44c05b3171df3cb083f8378f0
TonyShark007 <mauritzlemgen@gmail.com>
2021-05-12 19:01:53 +0200

    update

--- Code.py
+++ Code.py
@@ -13,16 +13,23 @@ L = 0
 with open("Js.json") as f:
     data = json.load(f)
 
+def logout():
+    data['Cache']['Angemeldet'] = "False"
+    with open("Js.json", "w+") as f:
+        json.dump(data, f, indent=4)
+    login()
+
 def desktop():
     global L
-    print("Willkommen bei Lit-Bot")
+    print("\nWillkommen bei Lit-Bot")
+    print(f"Sie sind angemeldet als: {data['Cache']['Name']}")
     E  = input("<> ")
     if E == "/help":
         pass
     elif E == "/kill":
         sys.exit()
     elif E == "/logout":
-        login()
+        logout()
     else:
         if L >= 4:
             cls()
@@ -45,43 +52,62 @@ def signup():
 
 def login():
     global H
-    cls()
-    V = input("Bitte Benutzer auswählen: ")
-    if V in data['Benutzer']:
-        index = data['Benutzer'].index(V)
-        User = data['Benutzer'][index]
-        Passwort = data['Passwort'][index]
-        if V == User:
-            if Passwort == "-":
-                desktop()
-            else:
-                P = input(f"\nBitte das Passwort für den Benuzter ({User}) eingeben: ")
-                if P == Passwort:
+    if data['Cache']['Angemeldet'] == "True":
+        desktop()
+    elif data['Cache']['Angemeldet'] == "False":
+        V = input("Bitte Benutzer auswählen: ")
+        if V in data['Benutzer']:
+            index = data['Benutzer'].index(V)
+            User = data['Benutzer'][index]
+            Passwort = data['Passwort'][index]
+            if V == User:
+                if Passwort == "-":
+                    data['Cache']['Angemeldet'] = "True"
+                    data['Cache']['Name'] = f"{V}"
+                    with open("Js.json", "w+") as f:
+                        json.dump(data, f, indent=4)
                     desktop()
                 else:
-                    cls()
-                    print("Falsches Passwort. Bitte erneut versuchen")
-                    time.sleep(2)
-                    cls()
+                    P = input(f"\nBitte das Passwort für den Benuzter ({User}) eingeben: ")
+                    if P == Passwort:
+                        data['Cache']['Angemeldet'] = "True"
+                        data['Cache']['Name'] = f"{V}"
+                        with open("Js.json", "w+") as f:
+                            json.dump(data, f, indent=4)
+                        desktop()
+                    else:
+                        cls()
+                        print("Falsches Passwort. Bitte erneut versuchen")
+                        time.sleep(2)
+                        cls()
+                        login()
+            elif V == "/help":
+                print("Falls du keinen Account hast gib '/new' oder 'local' als Benutzernamen ein\n")
+                login()
+            elif V == "/new":
+                try:
+                    signup()
+                except:
+                    print("International Error", end="\n")
                     login()
         elif V == "/help":
-            print("Falls du keinen Account hast gib '/new' oder 'local' als Benutzernamen ein\n")
-            login()
+                print("Falls du keinen Account hast gib '/new' oder 'local' als Benutzernamen ein\n")
+                login()
         elif V == "/new":
             try:
                 signup()
             except:
                 print("International Error", end="\n")
                 login()
-    elif V == "/help":
-            print("Falls du keinen Account hast gib '/new' oder 'local' als Benutzernamen ein\n")
-            login()
-    elif V == "/new":
-        try:
-            signup()
-        except:
-            print("International Error", end="\n")
-            login()
+            else:
+                if H >= 4:
+                    cls()
+                    print("Für Hilfe gib /help ein \n")
+                    login()
+                else:
+                    print(f"Nicht regestrierter Benutzer: <{V}> \n")
+                    H += 1
+                    login()
         else:
             if H >= 4:
                 cls()
@@ -92,13 +118,7 @@ def login():
                 H += 1
                 login()
     else:
-        if H >= 4:
-            cls()
-            print("Für Hilfe gib /help ein \n")
-            login()
-        else:
-            print(f"Nicht regestrierter Benutzer: <{V}> \n")
-            H += 1
-            login()
+        print("International Error. Bitte versuche es erneut")
+        login()
 
 login()
